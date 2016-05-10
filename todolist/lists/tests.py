cfeb486fd4da2a4bb613c6ce62d1b022cc3bdd9b
tingbaozhao <zhaotingbao@sina.cn>
2016-05-11 00:03:21 +0800

    增加两个测试用例

--- todolist/lists/tests.py
+++ todolist/lists/tests.py
@@ -1,7 +1,25 @@
+# coding: utf-8
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 
+from lists.views import home_page
 
 # Create your tests here.
 class SmokeTest(TestCase):
+
     def test_demo_001(self):
-        self.assertTrue(1 + 2 == 4)
+        self.assertTrue(1 + 2 == 3)
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertTrue(found.func == home_page)
+
+    def test_homepage_view_content(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith("<html>"))
+        self.assertTrue(response.content.endswith("</html>"))
--- todolist/lists/views.py
+++ todolist/lists/views.py
@@ -1,3 +1,8 @@
+# coding: utf-8
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home_page(request):
+    return HttpResponse("<html><b>黑乎乎</b></html>")
